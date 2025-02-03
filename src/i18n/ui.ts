7348f9ddc852125483dd93422f8654f963f478b5
rolemadelen <yjii@pm.me>
2025-02-03 12:49:41 -0800

    home.text modified

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -8,7 +8,8 @@ export const languages = {
   export const ui = {
     en: {
       'home.name': 'Jii Yoo',
-      'home.text': 'Recording fragments of thoughts and scattered knowledge.',
+      'home.text': "The more you know, the more creative you can be.",
+      // 'home.text': 'Recording fragments of thoughts and scattered knowledge.',
       'home.quote': '기록하기를 좋아하는 인간에게 축복을.',
       'home.menu1': {
         'title':'Writing',
