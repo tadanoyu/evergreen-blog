a977930cd5ea2004a28cb54c20af540b3c2c5285
rolemadelen <yjii@pm.me>
2025-04-26 12:14:47 -0700

    home page - 한글화

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -7,17 +7,17 @@ export const languages = {
   
   export const ui = {
     en: {
-      'home.name': 'Jii Yoo',
-      'home.text': "The more you know, the more creative you can be.",
-      // 'home.text': 'Recording fragments of thoughts and scattered knowledge.',
+      'home.name': '지원',
+      'home.username': 'manabeur',
+      'home.text': "안녕하세요 (・_・)ノ",
       'home.quote': '기록하기를 좋아하는 인간에게 축복을.',
       'home.menu1': {
-        'title':'Writing',
-        'exp': 'space for sharing my story'
+        'title':'끄적임',
+        'exp': '생각을 정리하고 기록하는 공간'
       },
       'home.menu2': {
-        'title':'Note',
-        'exp': 'space for my second brain'
+        'title':'이야기',
+        'exp': '내 이야기를 나누는 공간'
       },
     },
   } as const;
