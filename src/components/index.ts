161fc6dcca7695481f679e56d857de27c0d6888a
rolemadelen <yjii@pm.me>
2025-05-04 20:54:56 -0700

    refactoring

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as BaseHead } from './BaseHead.astro';
+export { default as Backlinks } from './Backlinks.astro';
+export { default as Footer } from './Footer.astro';
+export { default as Giscus } from './Giscus.astro';
+export { default as FormattedDate } from './FormattedDate.astro';
